f9c10cb9b770af1ebfd998a47339e4bc9265bdf6
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-01-26 16:43:40 -0500

    selection sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -42,17 +42,3 @@ void bubble_sort(int *array, size_t size)
 		size--;
 	}
 }
-	/*while (bubbly == true)
-	{
-		bubbly = false;
-		for (i = 0; i < len - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				swap_ints(array + i, array + i + 1);
-				print_array(array, size);
-				bubbly = true;
-			}
-		}
-		len--;
-	}*/
